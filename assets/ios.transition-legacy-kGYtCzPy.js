4134fe75ce66eebf6760633e4ef7055654b64714
PiFord <cliffordjohnburdeoslutrago@gmail.com>
2025-03-10 22:12:34 +0800

    Updates

--- assets/ios.transition-legacy-kGYtCzPy.js
+++ assets/ios.transition-legacy-kGYtCzPy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DxYpnUlL.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-DExr59I9.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-FmrSgvfQ.js
+++ assets/status-tap-legacy-FmrSgvfQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DxYpnUlL.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-DExr59I9.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
