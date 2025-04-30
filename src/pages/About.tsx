cd634dc269585ea55ca7bc1fb66e43134bba05af
PiFord <cliffordjohnburdeoslutrago@gmail.com>
2025-04-30 20:54:13 +0800

    Changes on about.tsx ioncard contents

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -13,6 +13,7 @@ import {
       IonTitle, 
       IonToolbar 
   } from '@ionic/react';
+  import warcraft1 from '../../img/warcraft1.jpg';
   
   const About: React.FC = () => {
     return (
@@ -27,13 +28,16 @@ import {
         </IonHeader>
         <IonContent className='ion-padding'>
         <IonCard className="profile-card">
-        <img alt="Clifford's profile picture" src='https://scontent.fmnl14-1.fna.fbcdn.net/v/t39.30808-1/480516990_3025784607577266_4596026472426777348_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=103&ccb=1-7&_nc_sid=1d2534&_nc_eui2=AeHKkW9zrYpQ6m5taGZI0FEzPpgTOgK1BwQ-mBM6ArUHBHgIcIIt7iJC7egOLwulwg-iwwpnL8mF7UF_gLnqkqg7&_nc_ohc=5nDD3fmMDLEQ7kNvgH3fyHu&_nc_zt=24&_nc_ht=scontent.fmnl14-1.fna&_nc_gid=ARbPqRj9cyeLZG7n3BQuyfc&oh=00_AYFAKh9VfyQN-oQEjf1uzb8uNZhzq4WOG64KjoyD61LZbg&oe=67D4A51B' />
+        <IonImg src={warcraft1} alt="WARCRAFT" />
       <IonCardHeader>
-        <IonCardTitle>Clifford John B. Lutrago</IonCardTitle>
-        <IonCardSubtitle>cjfordmoko</IonCardSubtitle>
+        <IonCardTitle>WarCraft</IonCardTitle>
+        <IonCardSubtitle></IonCardSubtitle>
       </IonCardHeader>
 
-      <IonCardContent>You can't undo past but you can do a better future</IonCardContent>
+      <IonCardContent>This epic fantasy artwork depicts a powerful, armored warrior slamming a massive,
+         rune-engraved sword into a monstrous foe. With a fierce expression and a flowing red cape, 
+         the warrior radiates strength and defiance as ghostly figures loom in the background. 
+        The scene captures the climax of a brutal battle between light and darkness.</IonCardContent>
     </IonCard>
         </IonContent>
       </IonPage>
