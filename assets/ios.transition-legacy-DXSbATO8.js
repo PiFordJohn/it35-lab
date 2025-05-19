a92c8c72426cc49a1f39fc6214dd71912ff7d80e
LutragoacliffordIT24A <cliffordjohnburdeoslutrago@gmail.com>
2025-05-20 05:02:20 +0800

    Updates

--- assets/ios.transition-legacy-DXSbATO8.js
+++ assets/ios.transition-legacy-DXSbATO8.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CVT9YzZ4.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CefkbPT-.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
