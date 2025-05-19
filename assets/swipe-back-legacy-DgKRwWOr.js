1747a445397df5bde01e8e04037f90000d3b17d6
LutragoacliffordIT24A <cliffordjohnburdeoslutrago@gmail.com>
2025-05-20 04:55:38 +0800

    Updates

--- assets/swipe-back-legacy-DgKRwWOr.js
+++ assets/swipe-back-legacy-DgKRwWOr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CN1VpnYZ.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CVT9YzZ4.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
